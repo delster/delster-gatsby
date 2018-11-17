92761313d93501db88b12963043b1edd5e24c10c
David Elster <davidandtheweb@gmail.com>
2018-11-17 18:37:27 -0500

    7. Programmatically create pages from data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { graphql } from "gatsby";
-import { css } from "react-emotion";
-import { rhythm } from "../utils/typography";
-import Layout from "../components/layout";
+import React from "react"
+import { css } from "react-emotion"
+import { Link, graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
 
 export default ({ data }) => {
-  console.log(data);
   return (
     <Layout>
       <div>
@@ -20,27 +19,37 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+            <Link
+              to={node.fields.slug}
               className={css`
-                margin-bottom: ${rhythm(1 / 4)};
-              `}
+                text-decoration: none;
+                color: inherit;
+              `
+              }
             >
-              {node.frontmatter.title}{" "}
-              <span
+
+              <h3
                 className={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  className={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
     </Layout>
-  );
-};
+  )
+}
 
 export const query = graphql`
   query {
@@ -53,9 +62,12 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
           }
+          fields {
+            slug
+          }
           excerpt
         }
       }
     }
   }
-`;
+`
